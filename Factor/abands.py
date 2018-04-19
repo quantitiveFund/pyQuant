386936daee50e270c696fc17352ddc4e5c7f138a
Ni He <ni.he@qq.com>
2018-04-20 00:40:45 +0800

    Create abands.py

---
+++ Factor/abands.py
@@ -0,0 +1,14 @@
+'''
+Acceleration Bands (ABANDS)
+Description
+
+The Acceleration Bands (ABANDS) created by Price Headley plots upper and lower envelope bands around a simple moving average. The width of the bands is based on the formula below.
+Formula
+
+Upper Band = Simple Moving Average (High * ( 1 + 4 * (High - Low) / (High + Low)))
+
+Middle Band = Simple Moving Average
+
+Lower Band = Simple Moving Average (Low * (1 - 4 * (High - Low)/ (High + Low)))
+'''
+
