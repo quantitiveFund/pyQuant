01edc6b33ff6cbab1a0b7d8f472dc15c958b957d
xinihe <ni.he@qq.com>
2018-05-03 21:44:01 +0800

    update

--- Database/loadfromMysql.py
+++ Database/loadfromMysql.py
@@ -11,9 +11,17 @@ import time
 
 def loadfromMysql(code, start_date, end_date=time.strftime('%Y-%m-%d',time.localtime(time.time())), paralist = ['Date','Close']):
     #连接MySQL数据库
-    conn = mysql.connector.connect(host="localhost",port=3306,user="root",\
-                       password="*****",database="test",charset="utf8")
-    cur = conn.cursor()
+    while True:    
+        pw = input('Please enter the password: ')
+        try:
+            conn = mysql.connector.connect(host='10.23.0.2', port=3306, user='root', password = pw, database='quant')  
+            cur = conn.cursor()
+            break
+        except:
+            re_try = input('The password might be wrong, or the datebase is not available, do you want to retry (y/n)? ')
+            if re_try == 'n' or re_try == 'no':
+                break
+
     ## 注意： where 从句里面输入的因子数值必须要加上单引号 where Date > '%s'"， 否则该where从句没有作用！！
     sql = "select %s from stock_%s where Date > '%s' and Date < '%s'" % \
     (','.join(paralist), code, start_date, end_date)
@@ -25,16 +33,15 @@ def loadfromMysql(code, start_date, end_date=time.strftime('%Y-%m-%d',time.local
         conn.close()
         return dat
     except:
-        print('Error: unable to fetch data')
-
-
+        print('Error: unable to fetch data. You might enter a wrong code')
 
 
 #### 函数输入部分
-#code = '600000'
-#start_date = '2017-05-01'
-#end_date = '2018-03-05'
-##paralist = ['Date','Open','High','Low','Close','ChgAmount',\
-##            'ChgRate','Volume','VolTrans','Swing','Turnover']
-#paralist = ['Date','Close']
-####
+code = '600000'
+start_date = '2017-05-01'
+end_date = '2018-03-05'
+#paralist = ['Date','Open','High','Low','Close','ChgAmount',\
+#            'ChgRate','Volume','VolTrans','Swing','Turnover']
+paralist = ['Date','Close']
+dat = loadfromMysql(code, start_date, end_date, paralist = paralist)
+###
