a071b4dedab825091e1580b5f01a3a4290bb376b
Ni He <ni.he@qq.com>
2018-05-05 16:06:29 +0800

    update

--- Measure/standard_K_show.py
+++ Measure/standard_K_show.py
@@ -21,7 +21,7 @@ import time
 def loadfromMysql(code, start_date, end_date=time.strftime('%Y-%m-%d',time.localtime(time.time())), paralist = ['Date','Close']):
     #连接MySQL数据库
     while True: 
-        hostname = input('Please enter the name of data server: ')
+        hostname = 'localhost'
         pw = input('Please enter the password: ')
         try:
             conn = mysql.connector.connect(host = hostname, port=3306, user='root', password = pw, database='quant')  
@@ -47,16 +47,25 @@ def loadfromMysql(code, start_date, end_date=time.strftime('%Y-%m-%d',time.local
         
         
 
-days = loadfromMysql(code ='603618', start_date = '2015-01-01', paralist = ['Date','Open','Close','High','Low'])
+days = loadfromMysql(code ='603618', start_date = '2015-01-01', paralist = ['Date','Open','Close','High','Low','Volume'])
 days = days.sort_index()
 days.index.name = 'DateTime'
 daysreshape = days.reset_index()
-# convert the datetime64 column in the dataframe to 'float days'
-daysreshape['DateTime']=mdates.date2num(daysreshape['DateTime'].astype(dt.datetime))
-daysreshape = daysreshape.reindex(columns=['DateTime','Open','High','Low','Close'])  
-    
-Av1 = movingaverage(daysreshape.Close.values, MA1)
-Av2 = movingaverage(daysreshape.Close.values, MA2)
+# convert the datetime64 column in the dataf rame to 'float days'
+for i in range(0,len(daysreshape['DateTime'])):
+    daysreshape['DateTime'][i]=mdates.date2num(dt.datetime.strptime(daysreshape['DateTime'][i], '%Y-%m-%d'))
+
+#daysreshape['DateTime']=mdates.date2num(daysreshape['DateTime'].astype(dt.datetime))
+daysreshape = daysreshape.reindex(columns=['DateTime','Open','High','Low','Close','Volume']) 
+#daysreshape[['Open','High','Close','Low','Volume']] = daysreshape[['Open','High','Close','Low','Volume']].apply(pd.to_numeric, errors='coerce')
+daysreshape[['Open','High','Close','Low','Volume']] = daysreshape[['Open','High','Close','Low','Volume']].astype(float) 
+
+MA1 = 10  
+MA2 = 50
+Av1  = daysreshape['Close'].rolling(window = MA1).mean().astype('float')  
+Av2  = daysreshape['Close'].rolling(window = MA2).mean().astype('float')  
+#Av1 = movingaverage(daysreshape.Close.values, MA1)
+#Av2 = movingaverage(daysreshape.Close.values, MA2)
 SP = len(daysreshape.DateTime.values[MA2-1:])
 fig = plt.figure(facecolor='#07000d',figsize=(15,10))
     
@@ -65,8 +74,8 @@ candlestick_ohlc(ax1, daysreshape.values[-SP:], width=.6, colorup='#ff1717', col
 Label1 = str(MA1)+' SMA'
 Label2 = str(MA2)+' SMA'
     
-ax1.plot(daysreshape.DateTime.values[-SP:],Av1[-SP:],'#e1edf9',label=Label1, linewidth=1.5)
-ax1.plot(daysreshape.DateTime.values[-SP:],Av2[-SP:],'#4ee6fd',label=Label2, linewidth=1.5)
+ax1.plot(daysreshape.DateTime.values[-SP:],Av1.values[-SP:],'#e1edf9',label=Label1, linewidth=1.5)
+ax1.plot(daysreshape.DateTime.values[-SP:],Av2.values[-SP:],'#4ee6fd',label=Label2, linewidth=1.5)
 ax1.grid(True, color='w')
 ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
@@ -81,41 +90,16 @@ ax1.tick_params(axis='x', colors='w')
 plt.ylabel('Stock price and Volume')
 plt.show()
 
- # plot an RSI indicator on top
-maLeg = plt.legend(loc=9, ncol=2, prop={'size':7},
-                       fancybox=True, borderaxespad=0.)
-maLeg.get_frame().set_alpha(0.4)
-textEd = pylab.gca().get_legend().get_texts()
-pylab.setp(textEd[0:5], color = 'w')
-    
-ax0 = plt.subplot2grid((6,4), (0,0), sharex=ax1, rowspan=1, colspan=4, axisbg='#07000d')
-rsi = rsiFunc(daysreshape.Close.values)
-rsiCol = '#c1f9f7'
-posCol = '#386d13'
-negCol = '#8f2020'
-    
-ax0.plot(daysreshape.DateTime.values[-SP:], rsi[-SP:], rsiCol, linewidth=1.5)
-ax0.axhline(70, color=negCol)
-ax0.axhline(30, color=posCol)
-ax0.fill_between(daysreshape.DateTime.values[-SP:], rsi[-SP:], 70, where=(rsi[-SP:]>=70), facecolor=negCol, edgecolor=negCol, alpha=0.5)
-ax0.fill_between(daysreshape.DateTime.values[-SP:], rsi[-SP:], 30, where=(rsi[-SP:]<=30), facecolor=posCol, edgecolor=posCol, alpha=0.5)
-ax0.set_yticks([30,70])
-ax0.yaxis.label.set_color("w")
-ax0.spines['bottom'].set_color("#5998ff")
-ax0.spines['top'].set_color("#5998ff")
-ax0.spines['left'].set_color("#5998ff")
-ax0.spines['right'].set_color("#5998ff")
-ax0.tick_params(axis='y', colors='w')
-ax0.tick_params(axis='x', colors='w')
-plt.ylabel('RSI')
 
+# Volume info
 volumeMin = 0
 ax1v = ax1.twinx()
-ax1v.fill_between(daysreshape.DateTime.values[-SP:],volumeMin, days.Volume.values[-SP:], facecolor='#00ffe8', alpha=.4)
+#ax1v.fill_between(daysreshape.DateTime.values[-SP:],volumeMin, days.Volume.values[-SP:].astype(int), facecolor='#00ffe8', alpha=.4)
+ax1v.fill_between(daysreshape.DateTime.values[-SP:].astype(int),volumeMin, days.Volume.values[-SP:].astype(int), facecolor='#00ffe8', alpha=.4)
 ax1v.axes.yaxis.set_ticklabels([])
 ax1v.grid(False)
     ###Edit this to 3, so it's a bit larger
-ax1v.set_ylim(0, 3*days.Volume.values.max())
+ax1v.set_ylim(0, 4*days.Volume.values.astype(int).max())
 ax1v.spines['bottom'].set_color("#5998ff")
 ax1v.spines['top'].set_color("#5998ff")
 ax1v.spines['left'].set_color("#5998ff")
